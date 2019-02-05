71ebf026c08392fda9bf8a62553796c040a77008
Tiffany Ray <tiffanyraylee@gmail.com>
2019-02-05 10:45:35 -0600

    updated docs folder and webpack with new dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,55 @@
 
-const path = require('path')
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'docs'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    //setting up the loader
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: [
-                /\.sass$/,
-                /\.css$/
-            ],
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'docs')
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'docs'),
+            filename: 'bundle.js'
+        },
+        //setting up the loader
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: [
+                    /\.sass$/,
+                    /\.css$/
+                ],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourcerMap: true
+                        }
+                    }
+                ]
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'docs')
+        }
     }
 };
+
 
